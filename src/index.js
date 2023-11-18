b521551408d999944be1724b0a7bea6dccf41502
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-18 14:21:16 -0300

    update acess

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    express.json()
+    next();
+  })
 const port = process.env.PORT || 3000;
 
 const New = mongoose.model('New', {
