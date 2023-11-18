3b3f0268c0f1a5c29c2b9535f50799832c2ed770
Matheus Alves <santalvesmatheus@gmail.com>
2023-11-17 22:25:50 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json())
+const port = 3000;
+
+const New = mongoose.model('New', {
+    title: String,
+    author: String,
+    url: String,
+    imageUrl: String
+})
+
+app.get("/", async (req, res) => {
+    const news = await New.find()
+    return res.send(news)
+})
+
+app.delete("/:id", async(req, res) => {
+    const _new = await New.findByIdAndRemove(req.params.id)
+    return res.send(_new)
+})
+
+app.put("/:id", async(req, res) => {
+    const _new = await New.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        author: req.body.author,
+        url: req.body.url,
+        imageUrl: req.body.imageUrl
+    }, {
+        new: true
+    })
+    return res.send(_new)
+})
+
+app.post("/", async (req, res) => {
+    const news = new New({
+        title: req.body.title,
+        author: req.body.author,
+        url: req.body.url,
+        imageUrl: req.body.imageUrl
+    })
+    await news.save()
+    return res.send(news)
+})
+
+app.listen(port, () => {
+    mongoose.connect('mongodb+srv://matalvessantana:JeqdiiUv7JJQrfYh@spacenewsapi.6ubmmbv.mongodb.net/?retryWrites=true&w=majority')
+
+    console.log('app is running')
+})
